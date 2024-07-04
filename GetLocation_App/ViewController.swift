c9c9f7b92dee09faddcad2e42c888fb8bbb080ba
Balakrishna <balakrishna.salla20@gmail.com>
2024-07-04 15:50:17 +0530

    Initial Commit

---
+++ GetLocation_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetLocation_App
+//
+//  Created by balakrishna salla on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
